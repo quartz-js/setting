c7bdab7932823450501ad2a93fd25fee05ef5b54
railken <railken@protonmail.ch>
2019-02-14 23:36:29 +0100

    update store setting

--- src/app/Api/UserSettingApi.js
+++ src/app/Api/UserSettingApi.js
@@ -8,10 +8,22 @@ export class UserSettingApi extends ResourceApi {
   resource_url = '/user/settings';
   
   storeByKey (key, value) {
-    return Vue.http.put(this.getFullUrl() + "?query=key eq '" + key + "'", { value: value }, { 
-      headers: { 
-        "Authorization": 'Bearer ' + this.access_token 
+    return this.index({query: "key eq '" + key + "'"}).then(response => {
+
+
+      if (response.body.data.length === 0) {
+        return Vue.http.post(this.getFullUrl(), { key: key, value: value }, { 
+          headers: { 
+            "Authorization": 'Bearer ' + this.access_token 
+          }
+        });
+      } else {
+        return Vue.http.put(this.getFullUrl() + "?query=key eq '" + key + "'", { value: value }, { 
+          headers: { 
+            "Authorization": 'Bearer ' + this.access_token 
+          }
+        });
       }
-    }).then(this.parse);
+    }).then(this.parse)
   }
 };
