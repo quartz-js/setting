c24e8c3e37bb747a01a6d1f25dd3f9b96e881c56
railken <railken@protonmail.ch>
2019-10-13 14:32:12 +0200

    remove includes

--- src/app/Api/UserSettingApi.js
+++ src/app/Api/UserSettingApi.js
@@ -4,15 +4,15 @@ export class UserSettingApi extends ResourceApi {
   resource_url = '/user/settings';
   
   retrieve (ownerId) {
-    return this.index({query: `ownables.owner_id = '${ownerId}`, show: 99999, include: 'ownables'})
+    return this.index({query: `ownables.owner_id = '${ownerId}`, show: 99999})
   }
 
   storeByKey (ownerId, key, value) {
-    return this.index({query: `ownables.owner_id = '${ownerId}' and key = '${key}'`, include: 'ownables'}).then(response => {
+    return this.index({query: `ownables.owner_id = '${ownerId}' and key = '${key}'`}).then(response => {
       if (response.body.data.length === 0) {
         return this.post('', { key: key, value: value });
       } else {
-        return this.http.put(this.url('' + `?query=ownables.owner_id = '${ownerId}' and key eq '${key}'`), { value: value, include: 'ownables' });
+        return this.http.put(this.url('' + `?query=ownables.owner_id = '${ownerId}' and key eq '${key}'`), { value: value });
       }
     })
   }
